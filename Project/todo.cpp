add777170705f4d001cea1763fc774e4c07b3b34
Jonathan <jonathancornejox@gmail.com>
2024-11-18 17:29:39 -0800

    driver program to add items and remove item

--- Project/todo.cpp
+++ Project/todo.cpp
@@ -2,7 +2,6 @@
 #include <string>
 
 // modify existing task
-// delete a task from existing list
 
 struct Node {
   std::string data;
@@ -25,15 +24,9 @@ public:
     if (head == nullptr) {
       head = tail = newNode;
     } else {
-      newNode->next = head->next;
-      newNode->prev = head;
-      if (head->next != nullptr) {
-        head->next->prev = newNode;
-      }
-      head->next = newNode; // link the next node of head to newNode
-      if (head == tail) {
-        tail = newNode;
-      }
+      tail->next = newNode;
+      newNode->prev = tail;
+      tail = newNode;
     }
   }
 
@@ -52,6 +45,28 @@ public:
       std::cout << "Item does not exist" << "\n";
       return;
     }
+    // if task is head
+    if (current == head) {
+      head = head->next;
+      if (head != nullptr) {
+        head->prev = nullptr;
+      }
+    } else {
+      // update previous node's next pointer
+      if (current->prev != nullptr) {
+        current->prev->next = current->next;
+      }
+    }
+    // if task is not head or tail
+    if (current->next != nullptr) {
+      current->next->prev = current->prev;
+    }
+
+    // if task is tail
+    if (current == tail) {
+      tail = current->prev;
+    }
+    delete current;
   }
 
   // display todo list items
@@ -78,15 +93,38 @@ public:
 
 int main() {
   TaskManagement taskManager;
+  std::string item;
+  int taskToDelete;
+  char userAns;
 
-  taskManager.addTask("My first task");
-  taskManager.addTask("Finish essay for writing class");
-  taskManager.addTask("Submit math exam");
+  std::cout << "Enter item to add to your TODO list: " << "\n";
 
-  std::cout << "Todo items in list: " << "\n";
+  std::cout << "Type quit to exit program" << "\n";
+  while (item != "quit") {
+
+    std::getline(std::cin, item);
+    if (item == "quit") {
+      continue;
+    }
+    taskManager.addTask(item);
+  }
+
+  std::cout << "List of TODO items: " << "\n";
   taskManager.showItems();
 
-  std::cout << "Removed item new list: " << "\n";
-  taskManager.deleteTask();
+  std::cout << "Do you want to delete an item? (y/n)" << "\n";
+  std::cin >> userAns;
+  if (userAns == 'y') {
+
+    std::cout << "Enter the number of the task to delete: ";
+    std::cin >> taskToDelete;
+    std::cout << "Deleting task " << taskToDelete << "..." << "\n";
+    taskManager.deleteTask(taskToDelete);
+    std::cout << "Updated todo list: " << "\n";
+    taskManager.showItems();
+  } else {
+    taskManager.showItems();
+  }
+
   return 0;
 }
